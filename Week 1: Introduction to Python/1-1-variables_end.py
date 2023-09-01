66bd5b0a86d9e2adb6b9567b74036a1485b62ce4
Hisham Abulnaga <h.abulnaga@gmail.com>
2023-09-02 01:46:30 +0300

    Add loops examples

---
+++ Week 1: Introduction to Python/1-1-variables_end.py
@@ -0,0 +1,4 @@
+# This file contains the complete task 1 for the topic of variables
+
+# Task 1: Define a variable called `name` and assign it the string "Bard".
+name = "Bard"
---
+++ Week 1: Introduction to Python/1-1-variables_start.py
@@ -0,0 +1,3 @@
+# This file contains the task 1 for the topic of variables
+
+# Task 1: Define a variable called `name` and assign it the string "Bard".
